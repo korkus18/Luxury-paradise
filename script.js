5521038de2a74befbe5bd44b60ba7e3a33914049
Jakub Korous <kubak.ko@seznam.cz>
2023-06-01 19:08:35 +0200

    dodelani sekci, hovery na buttony, funcnosti ruznych sekci

--- script.js
+++ script.js
@@ -1,10 +1,27 @@
-var toggleButton = document.getElementById("zobrazit-button");
-        var odpovedElement = document.querySelector(".odpoved");
+var toggleButton = document.querySelector(".zobrazit-button");
+var odpovedElement = document.querySelector(".odpoved");
 
-        toggleButton.addEventListener("click", function() {
-            if (odpovedElement.style.display === "none") {
-                odpovedElement.style.display = "block";
-            } else {
-                odpovedElement.style.display = "none";
-            }
-        });
+toggleButton.addEventListener("click", function() {
+  odpovedElement.classList.toggle("show");
+});
+
+
+
+
+function scrollPage() {
+    const scrollStep = window.innerHeight * 0.1;
+    const scrollInterval = setInterval(() => {
+        if (window.scrollY === document.documentElement.scrollHeight - window.innerHeight) {
+        clearInterval(scrollInterval);
+        } else {
+        window.scrollBy(0, scrollStep);
+        }
+    }, 20);
+    }
+    
+    const elements = document.getElementsByClassName("scrollElement");
+    Array.from(elements).forEach((element) => {
+    element.addEventListener("click", scrollPage);
+    });
+
+

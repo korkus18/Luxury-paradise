f7423e7d44d49ec57ecd8cf9eafb4f46990ecf88
Jakub Korous <kubak.ko@seznam.cz>
2023-06-02 01:05:29 +0200

    vytvoreni vsechy pages

--- script.js
+++ script.js
@@ -9,19 +9,19 @@ toggleButton.addEventListener("click", function() {
 
 
 function scrollPage() {
-    const scrollStep = window.innerHeight * 0.1;
-    const scrollInterval = setInterval(() => {
-        if (window.scrollY === document.documentElement.scrollHeight - window.innerHeight) {
-        clearInterval(scrollInterval);
-        } else {
-        window.scrollBy(0, scrollStep);
-        }
-    }, 20);
-    }
-    
-    const elements = document.getElementsByClassName("scrollElement");
-    Array.from(elements).forEach((element) => {
-    element.addEventListener("click", scrollPage);
-    });
+  const scrollStep = window.innerHeight * 0.1;
+  const scrollInterval = setInterval(() => {
+      if (window.scrollY === document.documentElement.scrollHeight - window.innerHeight) {
+      clearInterval(scrollInterval);
+      } else {
+      window.scrollBy(0, scrollStep);
+      }
+  }, 20);
+  }
+  
+  const elements = document.getElementsByClassName("scrollElement");
+  Array.from(elements).forEach((element) => {
+  element.addEventListener("click", scrollPage);
+});
 
 

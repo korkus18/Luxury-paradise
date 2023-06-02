a64cdbe2ac29762a029d314c7b77d163651345a8
Jakub Korous <kubak.ko@seznam.cz>
2023-06-02 15:09:48 +0200

    opravovani hero

--- script.js
+++ script.js
@@ -9,18 +9,18 @@ toggleButton.addEventListener("click", function() {
 
 
 function scrollPage() {
-  const scrollStep = window.innerHeight * 0.1;
+  const scrollStep = 600;
   const scrollInterval = setInterval(() => {
-      if (window.scrollY === document.documentElement.scrollHeight - window.innerHeight) {
+    if (window.scrollY === document.documentElement.scrollHeight - window.innerHeight) {
       clearInterval(scrollInterval);
-      } else {
+    } else {
       window.scrollBy(0, scrollStep);
-      }
+    }
   }, 10);
-  }
-  
-  const elements = document.getElementsByClassName("scrollElement");
-  Array.from(elements).forEach((element) => {
+}
+
+const elements = document.getElementsByClassName("scrollElement");
+Array.from(elements).forEach((element) => {
   element.addEventListener("click", scrollPage);
 });
 
@@ -29,5 +29,39 @@ function scrollPage() {
 
 
 
+var images = ["images/apartman-1.png", "images/apartman-2.png", "images/apartman-3.png"];
+        var gradient = "linear-gradient(0deg, rgba(0, 0, 0, 0.3), rgba(0, 0, 0, 0.3))";
 
+        document.body.style.background = `${gradient}, url(${images[0]}) no-repeat center / cover`;
 
+        var currentIndex = 0;
+        var backgroundElement = document.getElementById("background");
+        var buttons = document.getElementsByClassName("button");
+
+        function changeBackground(index) {
+            currentIndex = index;
+            backgroundElement.style.backgroundImage = "url('" + images[index] + "')";
+
+            for (var i = 0; i < buttons.length; i++) {
+                if (i === index) {
+                    buttons[i].classList.add("active");
+                } else {
+                    buttons[i].classList.remove("active");
+                }
+            }
+        }
+
+        function rotateBackground() {
+            currentIndex = (currentIndex + 1) % images.length;
+            backgroundElement.style.backgroundImage = "url('" + images[currentIndex] + "')";
+
+            for (var i = 0; i < buttons.length; i++) {
+                if (i === currentIndex) {
+                    buttons[i].classList.add("active");
+                } else {
+                    buttons[i].classList.remove("active");
+                }
+            }
+        }
+
+        setInterval(rotateBackground, 5000);

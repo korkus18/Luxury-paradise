6cbf9f4cb47472f37c791a8d51e5cd84dd63db4b
Jakub Korous <kubak.ko@seznam.cz>
2023-07-14 00:34:35 +0200

    opraveni scroll butonu

--- script.js
+++ script.js
@@ -26,6 +26,31 @@ Array.from(elements).forEach((element) => {
 
 
 
+function scrollDown() {
+  const currentPosition = window.pageYOffset;
+  const targetPosition = currentPosition + window.innerHeight;
+  const duration = 500 ; // Délka animace ve ms
+  const startTime = performance.now();
+
+  function scroll() {
+    const elapsed = performance.now() - startTime;
+    const progress = Math.min(elapsed / duration, 1);
+    const easing = easeOutCubic(progress);
+    const newPosition = currentPosition + (targetPosition - currentPosition) * easing;
+    window.scrollTo(0, newPosition);
+
+    if (progress < 1) {
+      requestAnimationFrame(scroll);
+    }
+  }
+
+  function easeOutCubic(t) {
+    return 1 - Math.pow(1 - t, 3);
+  }
+
+  requestAnimationFrame(scroll);
+}
+
 
 
 
